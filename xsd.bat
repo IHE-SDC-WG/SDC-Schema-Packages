2e9395208c0b2a97e5dda900ead4c9e9381b4585
Richard Moldwin <rmoldwi@cap.org>
2018-08-12 16:25:14 -0500

    Most recent versions of all rules language files, plus ancillary files.
    This branch is not active but may be used for reference and ancillary files.

---
+++ xsd.bat
@@ -0,0 +1,2 @@
+cd  "C:\Users\rmoldwi\Desktop\ONC-SDC\SDC Schemas Phase 2\"
+"C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\xsd.exe" sr-template2015_SDCcompat_5.xsd /c /l:vb
